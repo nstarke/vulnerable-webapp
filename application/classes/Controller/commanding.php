b3345b6c036016f17dc4b74aa23b6e0ce09fa52f
nstarke <starke.nicholas@gmail.com>
2015-04-09 20:31:36 -0500

    os commanding works

---
+++ application/classes/Controller/commanding.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Commanding extends Controller {
+
+	public function action_index()
+	{
+		$this->response->body(View::factory('commanding-index'));
+  }
+
+  public function action_submit()
+  {
+    $input = $_POST['input'];
+    $command = "ping -c 3 " . $input;
+    $command_output = shell_exec($command);
+    $this->response->body(View::factory('commanding')->bind('command_output', $command_output)->render());
+  }
+} // End Welcome
---
+++ application/views/commanding-index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>BSides Iowa 2015 Vulnerable Web app</title>
+    </head>
+    <body>
+        <h1>Ping an ip address</h1>
+        <form action="/commanding/submit" method="POST">
+            <input type="text" name="input">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
